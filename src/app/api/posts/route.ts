e01f5506cacae877933444868f4c85d606913396
warren <xxescapistx@gmail.com>
2024-08-15 01:29:58 +0400

    set revalidateto zero

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
+export const revalidate = 0;
+
 export const GET = async () => {
 
     const client = new PrismaClient()
 
-    const posts = await client.posts.findMany()
+    const posts = await client.posts.findMany();
 
   return new Response(
     JSON.stringify({
